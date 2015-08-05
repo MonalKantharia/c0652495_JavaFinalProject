2e035672225d71abae857a01620b8f61b8d020e5
Monal <Monal@10.99.53.179>
2015-08-05 16:28:46 -0400

    Servlet for changing the password of admin. AdminChangePasswordServlet java bean.

---
+++ src/java/attire/packages/AdminChangePasswordServlet.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attire.packages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Monal
+ */
+public class AdminChangePasswordServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        
+        String adminEmail = request.getParameter("adminEmail");
+        String adminPassword = request.getParameter("adminPassword");
+        String newAdminPassword = request.getParameter("newAdminPassword");
+        
+        Admin admin = new Admin(adminEmail, adminPassword);
+        
+        boolean check = admin.changePassword(newAdminPassword);
+        
+        
+        if(check)
+        {
+            out.println("Password Changed now!!");
+            out.println("<a href=\"adminHome.jsp\"> Go Home </a>");
+        }
+        
+        
+        else
+        {
+            out.println("Password Not Changed!!");
+            out.println("<a href=\"adminHome.jsp\"> Go Home </a>");
+        }
+    }
+    }
